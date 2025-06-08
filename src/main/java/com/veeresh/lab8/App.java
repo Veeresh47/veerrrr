68541e1915bf33558c6bb79fd5ffcbb5a0a6508c
Veeresh47 <veeruddabal@gmail.com>
2025-06-08 12:02:57 +0530

    Initial Commit

---
+++ src/main/java/com/veeresh/lab8/App.java
@@ -0,0 +1,13 @@
+package com.veeresh.lab8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
